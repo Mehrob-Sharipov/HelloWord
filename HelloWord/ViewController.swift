0e630c33e6210110595c37fd2cf1883838f0afaf
Mehrob <shmehrob16@gmail.com>
2022-07-23 17:13:06 +0500

    Add show/hide button

--- HelloWord/ViewController.swift
+++ HelloWord/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWordLable: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        helloWordLable.isHidden = true
+        toggleButton.layer.cornerRadius = 15
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func makeButtomAction() {
+        
+        if helloWordLable.isHidden {
+            helloWordLable.isHidden = false
+            toggleButton.setTitle("Hide text", for: .normal)
+        } else {
+            helloWordLable.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+        
+        
+    }
+    
 
 }
 
